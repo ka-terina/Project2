480256511c45ab3691080d08ddb9c621cfa89736
Katerina <popova.k.n@mail.ru>
2026-01-24 12:49:10 +0700

    add def filter

---
+++ src/processing.py
@@ -0,0 +1,11 @@
+def filter_by_state(new_lst: list, state="EXECUTED") -> list:
+    """Функция возвращает новый список словарей, содержащий только те
+    словари, у которых ключ state соответствует указанному значению"""
+    result = []
+    for i in new_lst:
+        if i["state"] == state:
+            result.append(i)
+
+    return result
+
+
