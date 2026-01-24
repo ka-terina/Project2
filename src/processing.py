7cb03c4ed12fe4d32fecfe9028f912c445d7cb30
Katerina <popova.k.n@mail.ru>
2026-01-24 15:30:23 +0700

    add def sort

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,21 @@
 def filter_by_state(new_lst: list, state="EXECUTED") -> list:
     """Функция возвращает новый список словарей, содержащий только те 
-    словари, у которых ключ state соответствует указанному значению""" result = []
+    словари, у которых ключ state соответствует указанному значению"""
+    result = []
     for i in new_lst:
         if i["state"] == state:
             result.append(i)
 
     return result
+
+
+def sort_by_date(new_lst: list, reverse=True) -> list:
+    """Функция возвращает новый список сортированный по дате"""
+    if reverse == True:
+        result = sorted(new_lst, key=lambda x: x["date"], reverse=True)
+    else:
+        result = sorted(new_lst, key=lambda x: x["date"], reverse=False)
+
+    return result
+
 
