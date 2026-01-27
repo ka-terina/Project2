daa5c8fc78e840306be68fd843a3c78589a54e27
Katerina <popova.k.n@mail.ru>
2026-01-27 14:00:52 +0700

    исправление

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(new_lst: list, state: str="EXECUTED") -> list:
+def filter_by_state(new_lst: list, state: str = "EXECUTED") -> list:
     """Функция возвращает новый список словарей, содержащий только те
     словари, у которых ключ state соответствует указанному значению"""
     result = []
@@ -9,7 +9,7 @@ def filter_by_state(new_lst: list, state: str="EXECUTED") -> list:
     return result
 
 
-def sort_by_date(new_lst: list, reverse: bool=True) -> list:
+def sort_by_date(new_lst: list, reverse: bool = True) -> list:
     """Функция возвращает новый список сортированный по дате"""
     result = sorted(new_lst, key=lambda x: x["date"], reverse=reverse)
 
