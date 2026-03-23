e2cf17f7d8db38a0d85fec56da062073b7c5d19d
Katerina <popova.k.n@mail.ru>
2026-03-23 15:28:17 +0700

    add test_filter

---
+++ src/processing.py
@@ -0,0 +1,17 @@
+def filter_by_state(new_lst: list, state: str = "EXECUTED") -> list:
+    """Функция возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению.
+    """
+    result =  []
+    for i in new_lst:
+        if i["state"] == state:
+            result.append(i)
+
+    return result
+
+
+def sort_by_date(new_lst: list, reverse: bool = True) -> list:
+    """Функция возвращает новый список сортированный по дате"""
+    result = sorted(new_lst, key=lambda x: x["date"], reverse=reverse)
+
+    return result
