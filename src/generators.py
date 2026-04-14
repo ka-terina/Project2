5777f02ebbd2646f7606be790266cf5bb8d44661
Katerina <popova.k.n@mail.ru>
2026-04-14 13:16:41 +0700

    исправила ошибки

--- src/generators.py
+++ src/generators.py
@@ -1,12 +1,31 @@
-def filter_by_currency(transactions: list, currency: str) -> dict:
+from collections.abc import Iterator
+
+def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
     """Фильтрует транзакции по заданной валюте"""
+
     for transaction in transactions:
-        if transaction["operationAmount"]["currency"]["name"] == currency:
+        transaction_currency = (
+            transaction
+            .get("operationAmount", {})
+            .get("currency", {})
+            .get("name")
+        )
+        if transaction_currency == currency:
             yield transaction
 
 
-def transaction_descriptions(transactions: list) -> str:
+def transaction_descriptions(transactions: list) -> Iterator[str]:
     """Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
     for transaction in transactions:
         yield transaction["description"]
+
+
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
+    """Выдает номера банковских карт в формате ХХХХ ХХХХ ХХХХ ХХХХ"""
+    for num in range(start, stop+1):
+        num_str = str(num)
+        num_str = "0" * (16 - len(num_str)) + num_str
+        parts = [num_str[i:i + 4] for i in range(0, 16, 4)]
+        formatted_card = " ".join(parts)
+        yield formatted_card
 
