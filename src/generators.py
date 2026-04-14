2e59cdb488eb1335c582445b4f97e34b3845dded
Katerina <popova.k.n@mail.ru>
2026-04-14 16:15:03 +0700

    исправила ошибки

--- src/generators.py
+++ src/generators.py
@@ -1,15 +1,11 @@
 from collections.abc import Iterator
 
+
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
     """Фильтрует транзакции по заданной валюте"""
 
     for transaction in transactions:
-        transaction_currency = (
-            transaction
-            .get("operationAmount", {})
-            .get("currency", {})
-            .get("name")
-        )
+        transaction_currency = transaction.get("operationAmount", {}).get("currency", {}).get("name")
         if transaction_currency == currency:
             yield transaction
 
@@ -24,10 +20,9 @@ def card_number_generator(start: int, stop: int) -> Iterator[str]:
     """Выдает номера банковских карт в формате ХХХХ ХХХХ ХХХХ ХХХХ"""
     if start == 0:
         start += 1
-    for num in range(start, stop+1):
+    for num in range(start, stop + 1):
         num_str = str(num)
         num_str = "0" * (16 - len(num_str)) + num_str
-        parts = [num_str[i:i + 4] for i in range(0, 16, 4)]
+        parts = [num_str[i: i + 4] for i in range(0, 16, 4)]
         formatted_card = " ".join(parts)
         yield formatted_card
-
--- tests/test_card_number_generator.py
+++ tests/test_card_number_generator.py
@@ -1,20 +1,19 @@
-import pytest
-from mypy.find_sources import create_source_list
-
 from src.generators import card_number_generator
 
-def test_card_number() ->None:
+
+def test_card_number() -> None:
     """Тест на корректную работу функции"""
     result = list(card_number_generator(1, 5))
-    assert result == ["0000 0000 0000 0001",
-                      "0000 0000 0000 0002",
-                      "0000 0000 0000 0003",
-                      "0000 0000 0000 0004",
-                      "0000 0000 0000 0005"]
+    assert result == [
+        "0000 0000 0000 0001",
+        "0000 0000 0000 0002",
+        "0000 0000 0000 0003",
+        "0000 0000 0000 0004",
+        "0000 0000 0000 0005",
+    ]
+
 
 def test_invalid_number() -> None:
     """Не корректный диапазон"""
-    result = list(card_number_generator(0,2))
+    result = list(card_number_generator(0, 2))
     assert result == ["0000 0000 0000 0001", "0000 0000 0000 0002"]
-
-
